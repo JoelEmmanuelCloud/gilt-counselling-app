9c1a0a2f3cb977ae3de188f5c577b9a69679c560
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-12-26 16:34:35 +0100

    Remove shopping bag icon from navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -155,12 +155,6 @@ const Navbar: React.FC = () => {
                   Book Session
                 </Link>
               )}
-
-              <button className="text-gray-700 hover:text-soft-terracotta transition-colors">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-                </svg>
-              </button>
             </div>
 
             {/* Mobile Menu Button */}
